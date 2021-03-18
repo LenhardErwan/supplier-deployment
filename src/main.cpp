4b960347c4c47dc9eae6f0d15795045af4e68e7d
LenhardErwan <35600901+LenhardErwan@users.noreply.github.com>
2021-03-18 11:54:39 +0100

    fix: space + rename output variable

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[]) {
 	if (argc >= 3) {
 		const std::string algorithm = argv[1];
 		const std::string input = argv[2];
-		const std::string ouput = (argc >= 4) ? argv[3] : "./output.txt";
+		const std::string output = (argc >= 4) ? argv[3] : "./output.txt";
 
 		SupplierDeployment test = SupplierDeployment();
 		test.readFile(argv[2]);
@@ -19,9 +19,9 @@ int main(int argc, char const *argv[]) {
 		std::cout << "Error during program initialization!" << std::endl;
 		std::cout << "Please respect the following syntax: " << std::endl << std::endl;
 		std::cout << "\tSupplierDeployment.exe <algorithm> <input> <?output>" << std::endl << std::endl;
-		std::cout << "-algorithm: greedy | greedyLocalSearch | linear | linearRandom" << std::endl;
-		std::cout << "-input: Path (relative or absolute) to the input file" << std::endl;
-		std::cout << "-output (optional): Path (relative or absolute) to the output file" << std::endl;
+		std::cout << "- algorithm: greedy | greedyLocalSearch | linear | linearRandom" << std::endl;
+		std::cout << "- input: Path (relative or absolute) to the input file" << std::endl;
+		std::cout << "- output (optional): Path (relative or absolute) to the output file" << std::endl;
 		return 1;
 	}
 
