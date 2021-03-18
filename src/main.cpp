47580f9e08ec6d790cc167acc0fa520f75869510
CastelJeremy <39135270+CastelJeremy@users.noreply.github.com>
2021-03-18 13:18:11 +0100

    feat: check which algorithm is selected

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 #include "SupplierDeployment.hpp"
 
 int main(int argc, char const *argv[]) {
-	if (argc >= 3) {
+	if (argc >= 3 &&
+		(argv[1] == std::string("greedy") ||
+		argv[1] == std::string("greedyLocalSearch") ||
+		argv[1] == std::string("linear") ||
+		argv[1] == std::string("linearRandom"))
+	) {
 		const std::string algorithm = argv[1];
 		const std::string input = argv[2];
 		const std::string output = (argc >= 4) ? argv[3] : "./output.txt";
@@ -11,8 +16,14 @@ int main(int argc, char const *argv[]) {
 		SupplierDeployment test = SupplierDeployment();
 		test.readFile(argv[2]);
 
-		if (argv[1] == "greedy") {
+		if (argv[1] == std::string("greedy")) {
 			test.greedy();
+		} else if (argv[1] == std::string("greedyLocalSearch")) {
+			test.greedyLocalSearch();
+		} else if (argv[1] == std::string("linear")) {
+			test.linear();
+		} else if (argv[1] == std::string("linearRandom")) {
+			test.linearRandom();
 		}
 
 		test.writeFile(output);
